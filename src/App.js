f9b57cc3f91e615c6ddbc33173d37581ab1c3c3e
nohatekate <119904831+nohatekate@users.noreply.github.com>
2023-05-09 13:23:25 -0500

    front end authorization (#2)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import React from 'react';
+import LoginButton from './components/LoginButton.js'
+import LogoutButton from './components/Logout.js'
+import Profile from './components/Profile.js'
 
 export default function App() {
   return (
     <div className="App">
-    <h1>Welcome to my app 👋</h1>
+      <h1>Welcome to my app 👋</h1>
+      <LoginButton />
+      <LogoutButton/>
+      <Profile/>
     </div>
   );
 }
