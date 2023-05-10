e06855d03ed5b757bde12e04b9109d18dc2729fa
nohatekate <kateiscoding@gmail.com>
2023-05-10 13:59:51 -0500

    set up react router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react'
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <Auth0Provider
     domain={domain}
     clientId={clientId}
@@ -13,6 +16,8 @@ root.render(
       redirect_uri: window.location.origin
     }}
   >
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Auth0Provider>
 );
