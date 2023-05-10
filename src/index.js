299ab78b31f1a78e17889bb7c3ac4a09731f63c2
nohatekate <119904831+nohatekate@users.noreply.github.com>
2023-05-10 14:00:43 -0500

    Reactrouter (#3)
    * install dependencies
    
    * set up react router

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -7,6 +8,7 @@ const domain = process.env.REACT_APP_AUTH0_DOMAIN
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <Auth0Provider
     domain={domain}
     clientId={clientId}
