0f5c55210137cf5a7dd4a873f2194ed779561053
nohatekate <119904831+nohatekate@users.noreply.github.com>
2023-05-10 19:13:14 -0500

    Mountainpage (#5)
    * partial json file mountain data
    
    * show mountains json data on home page, update hikes index page
    
    * refactor to give mountains their own page and keep home page for intro to app

--- src/components/Main/main.js
+++ src/components/Main/main.js
@@ -1,13 +1,18 @@
 import { Routes, Route } from 'react-router';
 import Hikes from '../../pages/Hikes/hikes'
 import Show from '../../pages/Show/show'
+import Mountains from '../../pages/Mountains/mountains.js';
+import Home from '../../pages/Home/home.js'
 
 export default function Main(props) {
     return (
         <main>
             <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/nh_48' element={<Mountains />} />
                 <Route path='/my-hikes' element={<Hikes />} />
                 <Route path='/hikes/:id' element={<Show />} />
+
             </Routes>
         </main>)
 }
---
+++ src/pages/Home/home.js
@@ -0,0 +1,9 @@
+
+
+export default function HOME(props){
+    return(
+        <div>
+        This will eventually be the landing page. I envision a log in / out option and a short description of what my app does.
+        </div>
+    )
+}
