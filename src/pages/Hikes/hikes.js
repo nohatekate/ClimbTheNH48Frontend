06ba797560bf25fe022ef2366361284a3e6db655
nohatekate <119904831+nohatekate@users.noreply.github.com>
2023-05-12 12:31:36 -0500

    Workingtowardmvp (#7)
    * update home page intro
    
    * brining hike details onto Mountain details page
    
    * refactor hikes detils function

--- src/pages/Hikes/hikes.js
+++ src/pages/Hikes/hikes.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
+// import { Link } from 'react-router-dom'
+import { HIKE_BASE_URL } from '../../utilities/constants'
 
 export default function Hikes(props) {
 
@@ -7,7 +9,7 @@ export default function Hikes(props) {
     const [hikes, setHikes] = useState([]);
     const { user, isAuthenticated } = useAuth0();
     console.log(user)
-    const BASE_URL = "http://localhost:4000/hike"
+
     // I need conditionals in here that say if the user has hikes then show the mountain with the hike details - if no hikes message to tel them to get hiking and check mountains off the list
     //if hiker === logged in user show all their hikes
     const getHikes = async () => {
@@ -15,7 +17,7 @@ export default function Hikes(props) {
             method: "GET"
         }
         try {
-            const response = await fetch(BASE_URL, options)
+            const response = await fetch(HIKE_BASE_URL, options)
             const allHikes = await response.json()
             console.log(allHikes)
             setHikes(allHikes)
@@ -41,21 +43,23 @@ export default function Hikes(props) {
                 return (
                     <div key={hike._id}>
                         <h1>{hike.mountain}</h1>
-                        <p>{hike.hiker}</p>
+                        <h2>{hike.date}</h2>
+                        <p>{hike.comments}</p>
+                        <p>{hike.summit}</p>
                     </div>)
             }
-            
-            
+
+
         })
-}
+    }
 
-return (
-    isAuthenticated &&
-    (<>
+    return (
+        isAuthenticated &&
+        (<>
 
-        <h1>🥾 Hikes Page 🥾 </h1>
-        {isLoading ? loading() : loaded()}
-    </>)
+            <h1>🥾 Hikes Page 🥾</h1>
+            {isLoading ? loading() : loaded()}
+        </>)
 
-)
+    )
 }
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,9 +1,9 @@
 
 
-export default function HOME(props){
-    return(
+export default function HOME(props) {
+    return (
         <div>
-        This will eventually be the landing page. I envision a log in / out option and a short description of what my app does.
+            Welcome to Climb the NH-48! This app was created so that you can track your hikes throughout the White Mountains. When you've summited all 48 4000 foot mountains don't forget to apply for your badge! <p>Please Log in to start keeping track of your hikes.</p>
         </div>
     )
 }
