52ab862ffa2b86c6ecfaa15d749cf6dcb3855bac
nohatekate <kateiscoding@gmail.com>
2023-05-14 17:36:34 -0500

    mountains page styled

--- src/pages/Mountains/mountains.js
+++ src/pages/Mountains/mountains.js
@@ -7,18 +7,22 @@ import mountainData from '../../mountain-data/nh48mountains.json';
 export default function Mountains(props) {
     const { isAuthenticated } = useAuth0();
     return (
-        <div>
+        <>
             <h1 className="mb-5 flex justify-center">The 48 NH 4000 footers</h1>
-            <div className='flex self-center flex-col space-y-5 '>
+            
                 {Object.entries(mountainData).map(([key, value]) => (
-                    <Link to={`/nh-48/${key}`} className={!isAuthenticated ? "disabled-link" : ""} key={key}>
-                        <div className='shadow-xl bg-tan p-5 m-1 max-w-2xl rounded-lg'>
-                            <p>{value.name}</p>
-                            <p>{value.elevation}ft</p>
-                        </div>
+                    <Link to={`/nh-48/${key}`} className={!isAuthenticated ? "disabled-link w-full max-w-2xl" : "w-full max-w-2xl"} key={key}>
+                    <div className='flex flex-col w-full shadow-xl bg-tan p-5 rounded-lg m-1 max-w-2xl mb-3'>
+                        
+                            <div className=''>
+                                <h2 className='font-medium'> Mt. {value.name}</h2>
+                                <p>Elevation | {value.elevation}ft</p>
+                            </div>
+                        
+                    </div>
                     </Link>
                 ))}
-            </div>
-        </div>
+
+        </>
     )
 }
