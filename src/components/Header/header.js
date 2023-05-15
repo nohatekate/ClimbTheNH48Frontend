d5747b3bcf92d2d2dddf1010b48019217af81c56
nohatekate <119904831+nohatekate@users.noreply.github.com>
2023-05-14 19:21:26 -0500

    Morestyling (#12)
    * working through styling mountains and detail pages and forms
    
    * making progress on styling
    
    * READme
    
    * fixed bug - redirect on delete
    
    * link to edit hike page from my-hikes page
    
    * getting hikes on my-hikes page by user rather than by mountain
    
    * hikes bug fixed - can now find user hikes on my hikes page and users mountain hikes on mountain details page
    
    * updated json mountain data to follow online amc4000footer.org
    
    * mountain detail page styled
    
    * mountains page styled
    
    * forms match styles

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -8,8 +8,8 @@ import { useAuth0 } from "@auth0/auth0-react";
 export default function Header(props) {
     const { isAuthenticated } = useAuth0();
     return (
-        <header>
-            <nav className="flex justify-between space-x-4 bg-darkest-green text-tan ">
+        <header className='shadow-md flex w-full justify-center'>
+            <nav className="flex justify-between space-x-4 bg-darkest-green text-tan w-full max-w-5xl">
                 <Link to={'/nh-48'} className={"rounded-lg px-3 py-2 text-tan font-medium hover:bg-tan hover:text-darkest-green ease-in-out duration-300 "}>NH-48</Link>
                 <div>
                     { isAuthenticated && <Link to={'/my-hikes'} className={"rounded-lg px-3 py-2 text-tan font-medium hover:bg-tan hover:text-darkest-green ease-in-out duration-300"}>My Hikes</Link>}
@@ -19,5 +19,6 @@ export default function Header(props) {
             </nav >
         </header >)
 }
+
 
 
